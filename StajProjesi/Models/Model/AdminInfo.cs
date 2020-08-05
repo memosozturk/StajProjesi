72c5539e2c0ec2a12b3e51d7bf5a267fffe25a24
Mehmet Öztürk <mozturk357@gmail.com>
2020-08-05 10:59:03 +0300

    admininfo

---
+++ StajProjesi/Models/Model/AdminInfo.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace StajProjesi.Models.Model
+{
+    [Table("Admin")]
+   
+ public class AdminInfo
+ {
+        [Key]
+        public int Adminid { get; set; }
+        [Required]
+        public String Eposta { get; set; }
+        [Required]
+        public String Sifre { get; set; }
+
+
+  }
+}
